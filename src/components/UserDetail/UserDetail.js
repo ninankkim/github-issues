7006caab695380053fb11c0c80c3e5879bd0b5b6
Nina Kim <ninakim0504@hotmail.com>
2020-11-05 20:18:02 -0500

    newfiles

--- src/components/UserDetail/UserDetail.js
+++ src/components/UserDetail/UserDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './UserDetail.js';
 
-export default function UserDetail(props) {
+export default function UserDetail() {
     const { login, avatar_url, bio, company, location, blog} = props.user;
     return (
         <div className="UserDetail">
-            
+
         </div>
     )
 
